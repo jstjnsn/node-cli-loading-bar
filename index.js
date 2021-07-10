e5daed0ce161c66600a988c0ba699b3d8437ae99
Joost Jansen <hello@joostjansen.me>
2021-07-10 10:44:20 +0200

    Add functionality to write a loading bar to the cli

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const readline = require('readline')
 
-readline.cursorTo(process.stdout, 3, 0)
-process.stdout.write("A")
+const size = 50
+let cursor = 0
+
+// console.clear()
+
+process.stdout.write("Loading...\n")
+
+process.stdout.write("\x1B[?25l")
+for (let i = 0; i < size; i++) {
+  process.stdout.write("\u2591")
+}
+
+readline.cursorTo(process.stdout, cursor, 2);
+
+timer = setInterval(() => {
+  process.stdout.write("\u2588")
+  cursor++;
+  if (cursor >= size) {
+    clearTimeout(timer)
+  }
+}, 100)
